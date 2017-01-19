f1b4ed56f74c9f5e78c6f46ae4bee5f7e87238a6
Sally Chen <chuhan.chen@mail.utoronto.ca>
2017-01-19 00:45:30 +0000

    fix migration file

--- db/migrate/20170115212851_create_ev_pictures.rb
+++ db/migrate/20170115212851_create_ev_pictures.rb
@@ -7,6 +7,6 @@ class CreateEvPictures < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_foreign_key :ev_pictures, :events
-    add_index :microposts, [:event_id, :created_at]
+    add_index :ev_pictures, [:event_id, :created_at]
   end
 end
