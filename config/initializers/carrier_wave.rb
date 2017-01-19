512311aaf95a36b2ec8dd5a19587d31f36795f03
Sally Chen <chuhan.chen@mail.utoronto.ca>
2017-01-19 02:31:38 +0000

    fix aws version

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,10 @@ if Rails.env.production?
       # Configuration for Amazon S3
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :aws_signature_version => 4
     }
+    
     config.fog_directory = ENV['S3_BUCKET']
   end
 end
