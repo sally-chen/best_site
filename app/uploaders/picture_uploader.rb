8fd2830eb5debfc1a715bd902f406fd12a06de4e
Sally Chen <chuhan.chen@mail.utoronto.ca>
2017-01-23 02:11:28 +0000

    resize event image from backend

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,6 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_fill: [800, 0]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
